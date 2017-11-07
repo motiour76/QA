4fe1024ff9eb77c0aadfa060018f04e8ec4644ae
motiour76@gmail.com <motiour76@gmail.com>
2017-11-07 16:20:11 -0500

    init

---
+++ BDDTest/src/test/java/com/studentapp/junit/studentsinfo/CreateStudentsDataDrivenTest.java
@@ -0,0 +1,18 @@
+package com.studentapp.junit.studentsinfo;
+
+import org.junit.Test;
+
+import com.studentapp.testbase.TestBase;
+
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Title;
+
+public class CreateStudentsDataDrivenTest extends TestBase{
+	
+	@Title("DataDriven test for adding multiple students to the students app")
+	@Test
+	public void createMultiplestudents() {
+		
+	}
+
+}
